ea5a303e38b320d1644c696b146c0223b8a401e8
Tanvir <tanvir@laptop>
2023-11-14 14:00:23 +0400

    feat: added function that draws ceiling and floor

--- sources/raycaster_engine.c
+++ sources/raycaster_engine.c
@@ -6,18 +6,23 @@
 /*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:39:46 by tanas             #+#    #+#             */
-/*   Updated: 2023/11/07 21:02:14 by tanas            ###   ########.fr       */
+/*   Updated: 2023/11/14 13:59:09 by tanas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-static int	get_color(int *colors)
+static int	get_color(int *colors, int transperancy, int endian)
 {
-	return ((colors[R] << 16) | (colors[G] << 8) | (colors[B]));
+	if (endian)
+		return ((colors[B] << 16) | (colors[G] << 8) | (colors[R])
+			| (transperancy << 24));
+	return ((transperancy << 24) | (colors[R] << 16) | (colors[G] << 8)
+		| (colors[B]));
+	
 }
 
-void	draw_ceiling_and_floor(t_img *img_data, t_map *map)
+void	draw_ceiling_and_floor(t_img *img_data, int *c_color, int *f_color)
 {
 	int	x;
 	int	y;
@@ -29,19 +34,20 @@ void	draw_ceiling_and_floor(t_img *img_data, t_map *map)
 		while (++x < WIN_WIDTH)
 		{
 			if (y < WIN_HEIGHT / 2)
-				my_pixel_put(img_data, x, y, get_color(map->c_color));
+				my_pixel_put(img_data, x, y, get_color(c_color, 0, img_data->endian));
 			else
-				my_pixel_put(img_data, x, y, get_color(map->f_color));
+				my_pixel_put(img_data, x, y, get_color(f_color, 0, img_data->endian));
 		}
 	}
 }
 
 void	start_raycaster(t_mlx *mlx_core)
 {
-	draw_minimap(mlx_core);
+	draw_ceiling_and_floor(&mlx_core->img_data, mlx_core->map->c_color, mlx_core->map->f_color);
+	// draw_minimap(mlx_core);
 
-	// draw_ceiling_and_floor(&mlx_core->img_data, mlx_core->map);
 	// get rays for horizontal and vertical intersections (DDA)
+	draw_rays_3D(mlx_core->map->grid, &mlx_core->player, &mlx_core->img_data);
 
 	mlx_put_image_to_window(mlx_core->mlx_ptr, mlx_core->window,
 		mlx_core->img_data.img_ptr, 0, 0);
