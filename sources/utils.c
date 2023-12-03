7f88bd9a3e45c3c075d0a4e398e04794312a77af
Tanvir <tanvir@laptop>
2023-12-04 00:23:49 +0400

    refactor: moved my_pixel_put to utils.c

--- sources/utils.c
+++ sources/utils.c
@@ -6,7 +6,7 @@
 /*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:45:11 by tanas             #+#    #+#             */
-/*   Updated: 2023/11/06 15:33:59 by tanas            ###   ########.fr       */
+/*   Updated: 2023/12/04 00:23:24 by tanas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,10 @@ int	write_error_msg(char *error_msg)
 	return (0);
 }
 
-/* -------------------------------- map utils ------------------------------- */
-void	print_t_map(t_map *map)
+/* ------------------------------- draw utils ------------------------------- */
+void	my_pixel_put(t_img *img_data, int x, int y, int color)
 {
-	if (!map)
-		return ;
-	printf("----------GRID------------\n");
-	for(int i = 0; i < map->n_rows; i++)
-		printf("%s", map->grid[i]);
-	printf("\n--------TEXTURES----------\n");
-	printf("NO_texture_fd = %d\n", map->NO_texture_fd);
-	printf("SO_texture_fd = %d\n", map->SO_texture_fd);
-	printf("WE_texture_fd = %d\n", map->WE_texture_fd);
-	printf("EA_texture_fd = %d\n", map->EA_texture_fd);
-	printf("---------COLORS-----------\n");
-	printf("floor	[R] = %d\n", map->f_color[R]);
-	printf("floor	[G] = %d\n", map->f_color[G]);
-	printf("floor	[B] = %d\n", map->f_color[B]);
-	printf("ceiling [R] = %d\n", map->c_color[R]);
-	printf("ceiling [G] = %d\n", map->c_color[G]);
-	printf("ceiling [B] = %d\n", map->c_color[B]);
+	if (y >= 0 && x >= 0 && x < WIN_WIDTH && y < WIN_HEIGHT)
+		*(unsigned int *)(img_data->addr + \
+			(y * img_data->line_length + x * (img_data->bpp / 8))) = color;
 }
