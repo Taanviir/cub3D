9a0acee9807cafa26c880e1c46bbfda98e88f260
Tanvir <tanvir@laptop>
2023-12-10 11:19:52 +0400

    style: Added necessary whitespaces in `map.c`

--- sources/map.c
+++ sources/map.c
@@ -6,7 +6,7 @@
 /*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 20:31:45 by tanas             #+#    #+#             */
-/*   Updated: 2023/12/10 01:31:46 by tanas            ###   ########.fr       */
+/*   Updated: 2023/12/10 11:19:43 by tanas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,9 +65,9 @@ t_map	*map_load(char *map_path)
 	if (!map)
 		return (close(map_fd), NULL);
 	if (!scene_load(map, map_fd, &current_map_row))
-		return(map_cleanup(current_map_row, map_fd, map));
+		return (map_cleanup(current_map_row, map_fd, map));
 	if (!grid_load(map, map_fd, &current_map_row))
-		return(map_cleanup(current_map_row, map_fd, map));
+		return (map_cleanup(current_map_row, map_fd, map));
 	return (close(map_fd), map);
 }
 
@@ -82,7 +82,7 @@ t_map	*map_load(char *map_path)
  *
  * @return (SUCCESS);
  */
-static void map_init_colors(t_map *map)
+static void	map_init_colors(t_map *map)
 {
 	int	i;
 
@@ -135,7 +135,7 @@ static int	grid_load(t_map *map, int map_fd, char **current_map_row)
 {
 	while (*current_map_row)
 	{
-		if(!grid_add_row(*current_map_row, map))
+		if (!grid_add_row(*current_map_row, map))
 			return (FAILURE);
 		*current_map_row = get_next_line(map_fd);
 	}
@@ -149,7 +149,7 @@ static int	grid_load(t_map *map, int map_fd, char **current_map_row)
  *
  * @return SUCCESS if the resizing is successful, FAILURE otherwise (e.g., memory allocation failure).
  */
-static int grid_resize(t_map *map)
+static int	grid_resize(t_map *map)
 {
 	char	**old_grid;
 	int		row_index;
@@ -158,7 +158,7 @@ static int grid_resize(t_map *map)
 	old_grid = map->grid;
 	map->grid_capacity *= DOUBLE;
 	map->grid = ft_calloc(map->grid_capacity + 1, sizeof(char *));
-	if(!map->grid)
+	if (!map->grid)
 	{
 		write_error_msg(MALLOC_FAIL);
 		return (FAILURE);
@@ -182,13 +182,13 @@ static int grid_resize(t_map *map)
  *
  * @return SUCCESS if the row is successfully added, FAILURE otherwise (e.g., resizing failure).
  */
-static int grid_add_row(char *current_map_row, t_map *map)
+static int	grid_add_row(char *current_map_row, t_map *map)
 {
 	if (!current_map_row)
 		return (FAILURE);
 	if (map->n_rows == map->grid_capacity)
 	{
-		if(!grid_resize(map))
+		if (!grid_resize(map))
 			return (FAILURE);
 	}
 	map->grid[map->n_rows++] = current_map_row;
@@ -232,7 +232,7 @@ static char	*scene_get_texture_id(char *current_map_row)
 static bool	scene_set_texture(char **texture, char *current_map_row)
 {
 	*texture = ft_substr(current_map_row, PATH_START, ft_strlen(current_map_row) - PATH_START);
-	if(!map_extension_check(*texture, ".xpm"))
+	if (!map_extension_check(*texture, ".xpm"))
 		return (FAILURE);
 	free(current_map_row);
 	if (*texture == FAILURE)
@@ -249,7 +249,7 @@ static bool	scene_set_texture(char **texture, char *current_map_row)
  *
  * @return An integer indicating success (SUCCESS) or failure (FAILURE).
  */
-static int scene_validate_color(char *color_string)
+static int	scene_validate_color(char *color_string)
 {
 	size_t	i;
 	size_t	comma_counter;
@@ -264,7 +264,7 @@ static int scene_validate_color(char *color_string)
 			comma_counter++;
 		if (!ft_is_digit(color_string[i])
 			&& color_string[i] != ',')
-		return (FAILURE);
+			return (FAILURE);
 		i++;
 	}
 	if (comma_counter != 2)
@@ -336,7 +336,7 @@ static int	scene_set(t_map *map, char *texture_id, char *current_map_row)
 	else if (!ft_strncmp(texture_id, "F", 1) && map->f_color[0] == NOT_SET)
 		return (scene_set_color(map->f_color, current_map_row));
 	else if (!ft_strncmp(texture_id, "C", 1) && map->c_color[0] == NOT_SET)
-		return(scene_set_color(map->c_color, current_map_row));
+		return (scene_set_color(map->c_color, current_map_row));
 	else
 		return (FAILURE);
 }
@@ -392,10 +392,10 @@ static int	scene_load(t_map *map, int map_fd, char **current_map_row)
 		if (!*current_map_row || !ft_is_alpha(*current_map_row[0]))
 			break ;
 		texture_id = scene_get_texture_id(*current_map_row);
-		if(!scene_set(map, texture_id, *current_map_row))
+		if (!scene_set(map, texture_id, *current_map_row))
 		{
 			free(texture_id);
-			return(write_error_msg(SCENE_FAIL));
+			return (write_error_msg(SCENE_FAIL));
 		}
 		free(texture_id);
 	}
@@ -414,7 +414,7 @@ static int	scene_load(t_map *map, int map_fd, char **current_map_row)
  * @param current_map_row Pointer to the current line being read from the file.
  * @param map_fd File descriptor of the map file.
  */
-static void empty_gnl(char *current_map_row, int map_fd)
+static void	empty_gnl(char *current_map_row, int map_fd)
 {
 	while (current_map_row)
 	{
@@ -498,7 +498,7 @@ static bool	map_row_is_empty(char *current_map_row)
 	while (current_map_row && *current_map_row)
 	{
 		if (!ft_is_whitespace(*current_map_row) && *current_map_row != '\n')
-			break;
+			break ;
 		else
 			current_map_row++;
 	}
@@ -522,7 +522,7 @@ static char	*map_next_valid_row(int map_fd)
 {
 	char	*current_map_row;
 
-	current_map_row = ft_calloc(1,sizeof(char *));
+	current_map_row = ft_calloc(1, sizeof(char *));
 	while (current_map_row && map_row_is_empty(current_map_row))
 	{
 		free(current_map_row);
